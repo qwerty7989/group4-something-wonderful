4bb97d1ec9989847a7a99aec607743f13c949e30
qwerty7989 <nattapat.ungk@gmail.com>
2023-02-06 16:24:24 +0700

    add CourseService

---
+++ complete/src/main/java/com/example/accessingdatamysql/controller/CourseController.java
@@ -0,0 +1,45 @@
+package com.example.accessingdatamysql.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.*;
+
+import com.example.accessingdatamysql.model.Course;
+import com.example.accessingdatamysql.service.CourseService;
+
+@Controller
+public class CourseController {
+
+	@Autowired
+	private CourseService courseService;
+
+	@PostMapping(path = "/courses") // Compliant
+	public @ResponseBody ResponseEntity<Object> addCourse(@RequestBody Course course) {
+
+		if(!(courseService.checkValid(course)))
+		Course result = courseService.save(course);
+
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("description_2", String.format("SAVED ID: %s!", result.getId()));
+		map.put("student", result);
+		return ResponseEntity.status(HttpStatus.OK).body(map);
+	}
+}
---
+++ complete/src/main/java/com/example/accessingdatamysql/service/repo/entity/CourseEntity.java
@@ -0,0 +1,90 @@
+package com.example.accessingdatamysql.service.repo.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="COURSES") // This tells Hibernate to make a table out of this class
+public class CourseEntity {
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="ID", updatable=false)	
+	private Integer id;
+
+	@Column(name="NAME")
+	private String name;
+
+	@Column(name="CREDIT")
+	private Integer credit;
+
+	@Column(name="MAXSEAT")
+	private Integer maxSeat;
+
+	@Column(name="COURSEID")
+	private Integer courseId;
+
+	@Column(name="CREATED")
+	private Timestamp created;
+
+
+
+
+
+	public StudentEntity() {
+	}
+	
+	public StudentEntity(Integer id, String name, Integer state) {
+		this.id = id;
+		this.name = name;
+		this.state = state;
+	}
+
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+
+	public Timestamp getCreated() {
+		return created;
+	}
+
+	public void setCreated(Timestamp created) {
+		this.created = created;
+	}
+
+
+}
