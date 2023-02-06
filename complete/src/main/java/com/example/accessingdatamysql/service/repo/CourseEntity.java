4e1745c20c89320305b5ed52fc73896120af2acd
zergreen <kawhao101@gmail.com>
2023-02-06 16:18:56 +0700

    course repository

---
+++ complete/src/main/java/com/example/accessingdatamysql/service/repo/CourseEntity.java
@@ -0,0 +1,15 @@
+package com.example.accessingdatamysql.service.repo;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.accessingdatamysql.service.repo.entity.CourseEntity;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface CourseRepository extends CrudRepository<CourseEntity, Integer> {
+
+    @Query("SELECT * FROM COURSES")
+    public Course queryCourse();    
+}
